8da7db64cbe1547bcc66037f64c85fdc64c59ca3
rririanto <romanticdevil.jimmy@gmail.com>
2021-04-27 02:55:36 +0700

    chore: add config

---
+++ .config/zsh/Darwin.zsh
@@ -0,0 +1,4 @@
+eval $(/usr/local/Homebrew/bin/brew shellenv)
+test -e "${HOME}/.iterm2_shell_integration.zsh" \
+    && source "${HOME}/.iterm2_shell_integration.zsh"
+export PATH="/opt/homebrew/opt/luajit-openresty/bin:$PATH"
